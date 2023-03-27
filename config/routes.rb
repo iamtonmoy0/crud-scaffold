40522e5338b5803cb7af8e67ef588aca61888639
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-27 19:52:32 -0400

    simple form added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'posts',to:'posts#index'
+  get 'posts/new',to:'posts#new'
 end
---
+++ db/migrate/20230327210654_add_body_to_post.rb
@@ -0,0 +1,4 @@
+class AddBodyToPost < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
