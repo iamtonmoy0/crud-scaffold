3337ac393be46bf70a2cd5c64929d565dfb3382f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-28 06:00:09 -0400

    post created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
 	def index
-		
+		@post= Post.all
 	end
 	def new
-		@post = Post.new()
+		@post = Post.new
+	end
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post,notice:"new post has created"
+		else
+			render:new,alert:"error while creating the post"
+		end
+	end
+	private
+	def post_params
+		params.require(:post).permit(:title,:body)
+		
 	end
 end
