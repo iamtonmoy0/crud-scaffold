64032b2659286697b917caccf829901dac0c9cba
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-27 20:02:30 -0400

    form created with 0 error

--- db/migrate/20230327210654_add_body_to_post.rb
+++
@@ -1,4 +0,0 @@
-class AddBodyToPost < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_210654) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_000106) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
